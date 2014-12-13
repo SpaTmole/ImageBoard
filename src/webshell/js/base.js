94414efa9150bfd551d0993f4d09fa4001602382
konstantin.oficerov <konstantin.oficerov@gmail.com>
2014-12-13 22:02:27 +0600

    add libraries, add new design, layout, change form requests and response parssings

--- src/webshell/js/base.js
+++ src/webshell/js/base.js
@@ -14,7 +14,35 @@
         initialize: function(attributes, options){
             this.url = options.protocol + "://" + options.host + "/rest/card/";
         },
-        model: CardModel
+        model: CardModel,
+        upload: function(attributes, options){
+            var self = this;
+            if(window.FormData) {
+                var data = new FormData();
+                $.each($(attributes.selector_photo)[0].files, function(i, file) {
+                    data.append('photo', file);
+                });
+                data.append("description", $(attributes.selector_description).val());
+                $.ajax({
+                    url: self.url,
+                    type: "POST",
+                    data: data,
+                    cache: false,
+                    contentType: false,
+                    dataType: 'json',
+                    processData: false,
+                    success: function (data) {
+                        if(options.success){
+                            options.success.apply(self, arguments);
+                        }
+                        console.log("success ", arguments)
+                    },
+                    error: function () {
+                        console.log("Upload failed ", arguments)
+                    }
+                })
+            } else console.error("Browser doesn't support async file transactions.")
+        }
     });
 
     document.CardModel = CardModel;
@@ -23,39 +51,107 @@
 })(jQuery, _, window.document, window);
 var host = "localhost:8080";
 $(document).ready(function(){
-    var carousel = $('.carousel');
-    carousel.carousel({interval: 0});
-    $('.left.carousel-control').on('click', function(){
-       carousel.carousel('prev');
+
+    var container = $('div.gallery').append('<ul></ul>').children();
+    var template = function (source, description, id){
+        var li = $('<li></li>');
+        var thumbnail = $('<span class="thumbnail"></span>');
+        var th_img = $('<img class="thumbnail-img"/>');
+        var actions = $('<span class="actions"></span>');
+        var rel = $('<a rel="prettyPhoto[main]"><i class="glyphicon glyphicon-search"></i></a>');
+        var rem = $('<a href="#"><i class="glyphicon glyphicon-remove"></i></a>');
+        var src_string = 'data:image/png;base64, '+source;
+
+        th_img.attr('src', src_string);
+        th_img.attr('alt', "#"+id);
+        rel.children().attr('alt', '#'+id);
+        rel.attr('href', src_string);
+        rel.attr('title', description);
+        actions.append(rel);
+        actions.append(rem);
+        thumbnail.append(th_img);
+        li.append(thumbnail);
+        li.append(actions);
+
+        rem.on('click', function(){
+            var el = $(this).parent().parent();
+            el.animate({opacity: 0}, 400, null, function(){el.remove()});
+
+        });
+
+
+        return li;
+    };
+
+    var modal = $('#uploadModal');
+    modal.modal({
+        show: false
     });
-    $('.right.carousel-control').on('click', function(){
-        carousel.carousel('next');
+    
+    var file_input = $('#photo');
+    var text_input = $('#description');
+    var preview_photo = $('#preview-photo');
+    var last_preview_blob = "";
+    file_input.on('change', function(){
+       if(file_input[0].files.length){
+           var file = $(file_input)[0].files.item(0);
+           var freader = new FileReader();
+           freader.onload = function(data){
+               preview_photo.attr('src', data.target.result);
+               preview_photo.fadeIn(400);
+               last_preview_blob = data.target.result;
+           };
+           freader.readAsDataURL(file);
+       } else {
+           preview_photo.removeAttr('src');
+           preview_photo.hide();
+           last_preview_blob = "";
+       }
+    });
+    var getFeeds = function(){
+        var result = new document.CardCollection({},{host: host, protocol: "http"});
+        result.fetch().done(function(){
+            $('#big-loader').hide();
+            for(var el in result.models) {
+                var model = result.models[el];
+                container.prepend(template(model.get('url'), model.get('description'), model.id || model.cid));
+                $("a[rel^='prettyPhoto']").prettyPhoto();
+            }
+        });
+
+        return result;
+    };
+    var Collection = getFeeds();
+
+    var upload_btn = $('#modal-upload-btn');
+    upload_btn.on('click', function(e){
+        e.preventDefault();
+        Collection.upload({selector_photo: file_input, selector_description: text_input},
+            {success: function(){
+                modal.modal('hide');
+                try{
+                    last_preview_blob = last_preview_blob.split(',')[1];
+                } catch(e){
+                    last_preview_blob = "";
+                }
+                if(last_preview_blob.length){
+                    container.prepend(template(last_preview_blob, text_input.val(), null));
+                    $("a[rel^='prettyPhoto']").prettyPhoto();
+                    Collection.add({
+                        url: last_preview_blob,
+                        description: text_input.val()
+                    });
+                }
+                file_input.val(null);
+                text_input.val("");
+
+            }})
     });
 
-    var Collection = new document.CardCollection({},{host: host, protocol: "http"});
-    Collection.fetch().done(function(){
-        $('#big-loader').hide();
-        $('#content-keeper').fadeIn(400);
-        for(var el in Collection.models){
-            var item = $('<div class="item"></div>'),
-                img = $('<img class="carousel-img">'),
-                caption = $('<div class="carousel-caption"></div>');
-            img.attr("src", 'data:image/png;base64, ' + Collection.models[el].get('url'));
-            caption.text(Collection.models[el].get('description'));
-            item.append(img, caption);
-            var indicator = $('<li data-target="#main-container"></li>');
-            indicator.data({
-                'slide-to': el
-            });
-            indicator.on('click', function(){
-                carousel.carousel(Number($(this).data('slide-to')));
-            });
-            if(el == 0){
-                item.addClass('active');
-                indicator.addClass('active');
-            }
-            $('.carousel-inner').append(item);
-            $('ol.carousel-indicators').append(indicator);
-        }
+    var refresh_btn = $('#refresh-btn');
+    refresh_btn.on('click', function(){
+        container.children().remove();
+        $('#big-loader').show();
+        Collection = getFeeds();
     });
 });
